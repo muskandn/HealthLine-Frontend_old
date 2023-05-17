22a6220c17aad2d2553d3261d4c96652492630ce
Muskan <muskandn02@gmail.com>
2023-05-17 19:08:41 +0530

    before redux done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
-import Header from "./component/layout/Header.js"
-import { BrowserRouter as Router } from 'react-router-dom';
+import Home from './component/Home/Home';
+import Footer from './component/layout/Footer/Footer';
+import Header from "./component/layout/Header/Header.js"
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+
+      </Routes>
+      
+      <Footer/>
     </Router>
     
   );
