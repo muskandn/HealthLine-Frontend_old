4ebeddd53872785b0d5ebb54236a9f791a6102b2
Muskan <muskandn02@gmail.com>
2023-05-28 12:40:41 +0530

    reset password done

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -209,6 +209,28 @@ export const forgotPassword = (email) => async (dispatch) => {
   }
 };
 
+
+// Reset Password
+export const resetPassword = (token, passwords) => async (dispatch) => {
+  try {
+    dispatch({ type: RESET_PASSWORD_REQUEST });
+
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.put(
+      `/api/v1/password/reset/${token}`,
+      passwords,
+      config
+    );
+
+    dispatch({ type: RESET_PASSWORD_SUCCESS, payload: data.success });
+  } catch (error) {
+    dispatch({
+      type: RESET_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 // Clearing Errors
 export const clearErrors = () => async (dispatch) => {
   dispatch({ type: CLEAR_ERRORS });
